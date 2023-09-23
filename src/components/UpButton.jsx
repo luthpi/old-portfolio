5ffe37d935cbceedd2964c5483d3becca5492c80
luthpai <105446118+luthpai@users.noreply.github.com>
2023-09-23 22:11:26 +0000

    a few changes

--- src/components/UpButton.jsx
+++ src/components/UpButton.jsx
@@ -7,7 +7,7 @@ const UpButton = () => {
     <a
       href="#home"
       id="upButton"
-      className="mb-[100vh] hover:scale-110 flex rounded-full p-3 w-[40px] h-[40px] flex-col items-center justify-center text-white bg-sky-600 hover:bg-sky-700 shadow-lg fixed"
+      className="mb-[100vh] hover:scale-110 flex rounded-full p-3 w-[40px] h-[40px] flex-col items-center justify-center text-white bg-sky-600 hover:bg-sky-700 shadow-lg text-xl fixed"
       role="button"
       aria-label="Up Button"
     >
