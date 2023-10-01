084457967356755d57fad25a0a79f351c5ae68f9
luthpai <luthfiafriansysah@gmail.com>
2023-10-01 19:18:16 +0700

    prettier

--- src/components/MobileNavbar.jsx
+++ src/components/MobileNavbar.jsx
@@ -1,10 +1,10 @@
 import React, { useState, useRef, useEffect } from "react";
 import { faClose, faBars } from "@fortawesome/free-solid-svg-icons";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
-import { AiOutlineHome } from 'react-icons/ai'
-import { BsPerson } from 'react-icons/bs'
-import { HiOutlineComputerDesktop } from 'react-icons/hi2'
-import { AiOutlineFolderOpen, AiOutlineMail } from 'react-icons/ai'
+import { AiOutlineHome } from "react-icons/ai";
+import { BsPerson } from "react-icons/bs";
+import { HiOutlineComputerDesktop } from "react-icons/hi2";
+import { AiOutlineFolderOpen, AiOutlineMail } from "react-icons/ai";
 
 const MobileNavbar = () => {
   const [nav, setNav] = useState(false);
@@ -30,52 +30,52 @@ text-3xl custom-transition"
             : "fixed right-0 w-[50vw] top-0 h-[100vh] bg-[#161616] lg:hidden flex flex-col justify-start items-center  lg:hidden shadow-2xl border-l-[2px] pt-16 border-gray-500 mnshadow box-border"
         }
       >
-      <ul className="flex justify-center items-start flex-col gap-3">
-        <li
-          onClick={navClick}
-          className="text-2xl custom-transition hover:text-gray-300 flex justify-between gap-2 items-center hover:scale-110"
-        >
-          <AiOutlineHome />
-          <a href="#home"> Home</a>
-        </li>
-        <li
-          onClick={navClick}
-          className="text-2xl custom-transition hover:text-gray-300 flex justify-between gap-2 items-center hover:scale-110"
-        >
-        <BsPerson />
-          <a href="#about"> About</a>
-        </li>
-        <li
-          onClick={navClick}
-          className="text-2xl custom-transition hover:text-gray-300 flex justify-between gap-2 items-center hover:scale-110"
-        >
-        <HiOutlineComputerDesktop />
-          <a href="#skills"> Skills</a>
-        </li>
-        <li
-          onClick={navClick}
-          className="text-2xl custom-transition hover:text-gray-300 flex justify-between gap-2 items-center hover:scale-110"
-        >
-        <AiOutlineFolderOpen />
-          <a href="#projects"> Projects</a>
-        </li>
-        <li
-          onClick={navClick}
-          className="text-2xl custom-transition hover:text-gray-300 flex justify-between gap-2 items-center hover:scale-110"
-        >
-        <AiOutlineMail />
-          <a href="#contact"> Contact</a>
-        </li>
-        <li
-          onClick={navClick}
-          className="text-3xl custom-transition absolute z-20 -left-6 p-3 rounded-full border-2 border-gray-500 w-[45px] h-[45px] bg-[#202020] flex items-center justify-center top-[30vh]"
-        >
-          <FontAwesomeIcon
-            className="custom-transition text-4xl"
-            icon={faClose}
-          />
-        </li>
-      </ul>
+        <ul className="flex justify-center items-start flex-col gap-3">
+          <li
+            onClick={navClick}
+            className="text-2xl custom-transition hover:text-gray-300 flex justify-between gap-2 items-center hover:scale-110"
+          >
+            <AiOutlineHome />
+            <a href="#home"> Home</a>
+          </li>
+          <li
+            onClick={navClick}
+            className="text-2xl custom-transition hover:text-gray-300 flex justify-between gap-2 items-center hover:scale-110"
+          >
+            <BsPerson />
+            <a href="#about"> About</a>
+          </li>
+          <li
+            onClick={navClick}
+            className="text-2xl custom-transition hover:text-gray-300 flex justify-between gap-2 items-center hover:scale-110"
+          >
+            <HiOutlineComputerDesktop />
+            <a href="#skills"> Skills</a>
+          </li>
+          <li
+            onClick={navClick}
+            className="text-2xl custom-transition hover:text-gray-300 flex justify-between gap-2 items-center hover:scale-110"
+          >
+            <AiOutlineFolderOpen />
+            <a href="#projects"> Projects</a>
+          </li>
+          <li
+            onClick={navClick}
+            className="text-2xl custom-transition hover:text-gray-300 flex justify-between gap-2 items-center hover:scale-110"
+          >
+            <AiOutlineMail />
+            <a href="#contact"> Contact</a>
+          </li>
+          <li
+            onClick={navClick}
+            className="text-3xl custom-transition absolute z-20 -left-6 p-3 rounded-full border-2 border-gray-500 w-[45px] h-[45px] bg-[#202020] flex items-center justify-center top-[30vh]"
+          >
+            <FontAwesomeIcon
+              className="custom-transition text-4xl"
+              icon={faClose}
+            />
+          </li>
+        </ul>
       </div>
     </>
   );
