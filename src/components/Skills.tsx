ee2fac0ddb3a78085a345277ee2e009b79126f44
luthpai <luthfiafriansysah@gmail.com>
2023-10-28 22:10:31 +0700

    too colorful

--- src/components/Skills.tsx
+++ src/components/Skills.tsx
@@ -1,6 +1,11 @@
 import React, { useState, useEffect, useRef } from "react";
 import { FaHtml5, FaCss3Alt, FaReact, FaSass } from "react-icons/fa";
-import { SiTailwindcss, SiPhp, SiBootstrap, SiVuedotjs } from "react-icons/si";
+import {
+  SiTailwindcss,
+  SiPhp,
+  SiBootstrap,
+  SiVuedotjs,
+} from "react-icons/si";
 import { BiLogoTypescript, BiLogoJavascript } from "react-icons/bi";
 
 const Skills = () => {
@@ -24,53 +29,29 @@ const Skills = () => {
       history.pushState({}, "", "#skills");
     }
   }, [isIntersecting]);
-
+  
   interface item {
-    id: number;
-    name: string;
-    ico: SVGElement;
-    text: string;
-    bg: string;
+    id: number, 
+    name: string,
+    ico: SVGElement,
+    text: string, 
+    bg: string
   }
-
+  
   const langs: item[] = [
-    { id: 1, name: "HTML", ico: FaHtml5, text: "white", bg: "#E34C26" },
-    { id: 2, name: "CSS", ico: FaCss3Alt, text: "white", bg: "#264DE4" },
-    {
-      id: 3,
-      name: "Javascript",
-      ico: BiLogoJavascript,
-      text: "white",
-      bg: "#FFA500",
-    },
-    {
-      id: 4,
-      name: "Typescript",
-      ico: BiLogoTypescript,
-      text: "white",
-      bg: "#358EF1",
-    },
-    { id: 5, name: "PHP", ico: SiPhp, text: "white", bg: "#474a8a" },
+    { id: 1, name: "HTML", ico: FaHtml5, text: "white", bg: "#303030" },
+    { id: 2, name: "CSS", ico: FaCss3Alt, text: "white", bg: "#303030" },
+    { id: 3, name: "Javascript", ico: BiLogoJavascript, text: "white", bg: "#303030" },
+    { id: 4, name: "Typescript", ico: BiLogoTypescript, text: "white", bg: "#303030" },
+    { id: 5, name: "PHP", ico: SiPhp, text: "white", bg: "#303030" },
   ];
 
   const fws: item[] = [
-    { id: 1, name: "SASS", ico: FaSass, text: "white", bg: "#cd6799" },
-    {
-      id: 2,
-      name: "Bootstrap",
-      ico: SiBootstrap,
-      text: "white",
-      bg: "#8E14FF",
-    },
-    {
-      id: 3,
-      name: "Tailwind",
-      ico: SiTailwindcss,
-      text: "white",
-      bg: "#38BDF8",
-    },
-    { id: 4, name: "React", ico: FaReact, text: "white", bg: "#61dbfb" },
-    { id: 5, name: "Vue", ico: SiVuedotjs, text: "white", bg: "#41b883" },
+    { id: 1, name: "SASS", ico: FaSass, text: "white", bg: "#303030" },
+    { id: 2, name: "Bootstrap", ico: SiBootstrap, text: "white", bg: "#303030" },
+    { id: 3, name: "Tailwind", ico: SiTailwindcss, text: "white", bg: "#303030" },
+    { id: 4, name: "React", ico: FaReact, text: "white", bg: "#303030" },
+    { id: 5, name: "Vue", ico: SiVuedotjs, text: "white", bg: "#303030" },
   ];
 
   return (
@@ -87,50 +68,50 @@ const Skills = () => {
       </h1>
       <div className="custom-transition box-border flex w-full flex-col items-start justify-center gap-2 px-5 md:items-stretch md:px-20">
         <div
-          className="mt-5 flex w-full flex-col items-center gap-2"
+          className="flex flex-col gap-2 mt-5 w-full items-center"
           data-aos="flip-left"
         >
           <h1 className="mb-2 text-[23px] font-semibold md:text-lg">
             Languages
           </h1>
-          <div className="flex w-full flex-wrap justify-center gap-2 rounded-lg px-4 py-1">
-            {langs.map((lang) => {
-              const Ico: React.ReactElement = lang.ico;
-              return (
-                <div
-                  className="border-box custom-transition flex items-center justify-center gap-3 rounded-md p-2 shadow-lg"
-                  style={{ color: lang.text, backgroundColor: lang.bg }}
-                  data-aos="fade-right"
-                  key={lang.key}
-                >
-                  <Ico className="text-[30px]" />
-                </div>
-              );
-            })}
-          </div>
+          <div className="flex px-4 rounded-lg flex-wrap justify-center py-1 w-full gap-2">
+          {langs.map((lang) => {
+            const Ico: React.ReactElement = lang.ico;
+            return (
+              <div
+                className="border-box custom-transition flex items-center justify-center gap-3 p-2 rounded-md shadow-lg"
+                style={{ color: lang.text, backgroundColor : lang.bg }}
+                data-aos="fade-right"
+                key={lang.key}
+              >
+                <Ico className="text-[30px]" />
+              </div>
+            );
+          })}
+        </div>
         </div>
         <div
-          className="mt-5 flex w-full flex-col items-center gap-2"
+          className="flex flex-col gap-2 mt-5 w-full items-center"
           data-aos="flip-left"
         >
           <h1 className="mb-2 text-[18px] font-semibold md:text-lg">
             Frameworks, Libraries, etc.
           </h1>
-          <div className="flex w-full flex-wrap justify-center gap-2 rounded-lg px-4 py-1">
-            {fws.map((fw) => {
-              const Ico: React.ReactElement = fw.ico;
-              return (
-                <div
-                  className="border-box custom-transition flex items-center justify-center gap-3 rounded-md p-2 shadow-lg"
-                  style={{ color: fw.text, backgroundColor: fw.bg }}
-                  data-aos="fade-right"
-                  key={fw.key}
-                >
-                  <Ico className="text-[30px]" />
-                </div>
-              );
-            })}
-          </div>
+          <div className="flex px-4 rounded-lg flex-wrap justify-center py-1 w-full gap-2">
+          {fws.map((fw) => {
+            const Ico: React.ReactElement = fw.ico;
+            return (
+              <div
+                className="border-box custom-transition flex items-center justify-center gap-3 p-2 rounded-md shadow-lg"
+                style={{ color: fw.text, backgroundColor : fw.bg }}
+                data-aos="fade-right"
+                key={fw.key}
+              >
+                <Ico className="text-[30px]" />
+              </div>
+            );
+          })}
+        </div>
         </div>
       </div>
     </div>
